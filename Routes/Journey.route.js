26bb4b3bb8d53218a05fdf5f5076595948c7310c
Ildiko Makra <N4689@student.jamk.fi>
2023-01-30 10:01:12 +0200

    Delete duplications

--- Routes/Journey.route.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const JourneyRouter = express.Router();
-const JourneyController = require("../Controllers/Journey.controller");
-
-// Routes after HOST/api/journeys
-JourneyRouter.get("/", JourneyController.getAllJourneys);
-
-module.exports = JourneyRouter;
