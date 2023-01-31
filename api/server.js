780a2f33c16ac7187d26647cefe311f568c0e02a
Ildiko Makra <N4689@student.jamk.fi>
2023-01-31 10:54:41 +0200

    Enable CORS on server side

--- api/server.js
+++ api/server.js
@@ -13,6 +13,20 @@ require("./connectDB")();
 // App
 const app = express();
 
+// Enable CORS
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "POST, PUT, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
+
 // Routes
 app.get("/", (req, res) => {
   res.send("Citybike API - Ildikó Makra -  v0.1.0");
