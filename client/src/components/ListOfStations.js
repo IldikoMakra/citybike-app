959e959769a1d6d2e997b77710f7ad2b1a6cbe42
Ildiko Makra <N4689@student.jamk.fi>
2023-02-04 13:05:55 +0200

    Fix data fetching on single station view

--- client/src/components/ListOfStations.js
+++ client/src/components/ListOfStations.js
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import SingleStation from "./SingleStation";
 
 function ListOfStations({ stations }) {
-  return (
+  const [clicked, setClicked] = useState(false);
+  const [name, setName] = useState("");
+
+  return clicked ? (
+    <SingleStation name={name} />
+  ) : (
     <div className="mx-auto">
       <div className="list-group">
         {stations.map((station) => (
-          <a
-            href={"/stations/" + station}
+          <button
             className="list-group-item"
             key={station.id}
+            onClick={() => {
+              setClicked(true);
+              setName(station.name);
+            }}
           >
             {station.name}
-          </a>
+          </button>
         ))}
       </div>
     </div>
--- client/src/components/SingleStation.js
+++ client/src/components/SingleStation.js
@@ -1,34 +1,50 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function SingleStation({ name }) {
   const [stationData, setStationData] = useState({});
 
+  useEffect(() => {
+    const url = "http://86.50.228.198/api/stations/" + name;
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
+        setStationData(json);
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
-    <div className="card w-100 mx-auto shadow p-3 mt-5 bg-body-tertiary rounded">
+    <div className="card w-75 mx-auto shadow p-3 mt-5 bg-body-tertiary rounded">
       <div className="card-body">
         <h3>{name} Station Details</h3>
         <div className="container mt-4">
           <div className="row pb-2">
-            <div className="col">Station Name</div>
+            <div className="col ">Station Name</div>
             <div className="col" data-cy="stationName">
               {stationData.name}
             </div>
           </div>
           <div className="row pb-2">
-            <div className="col">Station Address</div>
+            <div className="col ">Station Address</div>
             <div className="col">{stationData.address}</div>
           </div>
           <div className="row pb-2">
-            <div className="col">All Journeys Started Here</div>
+            <div className="col ">All Journeys Started Here</div>
             <div className="col">{stationData.departures}</div>
           </div>
           <div className="row">
-            <div className="col">All Journeys Ended Here</div>
+            <div className="col ">All Journeys Ended Here</div>
             <div className="col">{stationData.returns}</div>
           </div>
         </div>
       </div>
-      <a href="/stations">Back to Stations</a>
+      <a href="/stations" className="link-dark text-center mt-4">
+        Back to Stations
+      </a>
     </div>
   );
 }
